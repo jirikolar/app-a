a282e19c6678b7ad3bc1c760e6202bf410d00cc8
jirikolar <b2regs@gmail.com>
2016-05-05 21:03:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
   </head>
   <body>
     <div class="box">
-      <h1>A!!</h1>
+      <h1>A</h1>
       <h2><?php echo getenv('HOSTNAME') ?></h2>
       <hr>
-      <h2>!!refresh in:</h2>
+      <h2>refresh in:</h2>
       <h1 id="countdown">3</h1>
     </div>
   </body>
